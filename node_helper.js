fff9430aa4190a66af0994560b65e5906b8d2e6b
Harm Tilburgs <harm@tilburgs.com>
2026-03-10 21:14:55 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -50,7 +50,8 @@ module.exports = NodeHelper.create({
       }
 
       // CPU usage
-      let cpuUsage = await this.execCmd("top -bn1 | grep 'Cpu(s)' | awk '{print 100-$8}'");
+      // let cpuUsage = await this.execCmd("top -bn1 | grep 'Cpu(s)' | awk '{print 100-$8}'");
+      let cpuUsage = await this.execCmd("awk '/^%Cpu/{gsub(/,/, ".", $8); print 100 - $8}' <(top -b -n 1)");
       if (cpuUsage) data.cpuUsage = parseFloat(cpuUsage).toFixed(1) + "%";
 
       // Memory %
